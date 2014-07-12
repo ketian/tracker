a2fdd211b4d526390e30b322f0f00a4c3ec4d62d
beanxu <dearsummer@zju.edu.cn>
2014-07-12 21:55:44 +0800

    polish

---
+++ build/app/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/Config.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/Config.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/Features.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/Features.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/GraphUtils/GraphUtils.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/GraphUtils/GraphUtils.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/HaarFeature.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/HaarFeature.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/HaarFeatures.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/HaarFeatures.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/HistogramFeatures.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/HistogramFeatures.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/ImageRep.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/ImageRep.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/LaRank.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/LaRank.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/MultiFeatures.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/MultiFeatures.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/RawFeatures.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/RawFeatures.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/Sampler.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/Sampler.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Algorithm/Tracker.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Algorithm/Tracker.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Model/TrafficSignModel.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Model/TrafficSignModel.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Utility/ImageData.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Utility/ImageData.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/Utility/VideoData.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/Utility/VideoData.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/View/MainWindowSink.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/View/MainWindowSink.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/build/app/View/moc_traffic_signmainwindow.cxx" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/View/moc_traffic_signmainwindow.cxx.o"
+  "/home/ketian/Documents/code/traffic-sign/app/View/traffic_signmainwindow.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/View/traffic_signmainwindow.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/ViewModel/TrafficSignViewModel.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/ViewModel/TrafficSignViewModel.cpp.o"
+  "/home/ketian/Documents/code/traffic-sign/app/main.cpp" "/home/ketian/Documents/code/traffic-sign/build/app/CMakeFiles/main.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "app"
+  "../app/."
+  "../app/./Common"
+  "../app/./Model"
+  "../app/./Utility"
+  "../app/./View"
+  "../app/./ViewModel"
+  "/include"
+  "/usr/include/eigen2"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
